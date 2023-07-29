ef3ab6e6f0f5997f18a12111f4e7d99ed3ef8a38
Baloc <Baloc@users.noreply.github.com>
2023-07-29 16:55:58 +0200

    Revert "website/docs: copy edits on User docs"

--- website/docs/user-group/user/invitation.md
+++
@@ -1,5 +0,0 @@
----
-title: Create an invitation for users
----
-
-#TODO
--- website/sidebars.js
+++ website/sidebars.js
@@ -226,17 +226,7 @@ module.exports = {
         {
             type: "category",
             label: "Users & Groups",
-            items: [
-                {
-                    type: "category",
-                    label: "User",
-                    items: [
-                        "user-group/user/index",
-                        "user-group/user/user_basic_operations",
-                    ],
-                },
-                "user-group/group",
-            ],
+            items: ["user-group/user", "user-group/group"],
         },
         {
             type: "category",
